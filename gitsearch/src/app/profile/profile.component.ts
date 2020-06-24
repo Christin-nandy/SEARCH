7b5838681a85cfd3184c1014659bef364607ed49
Christin-nandy <achiengchristine14@gmail.com>
2020-06-24 23:36:27 +0300

    repos class

--- gitsearch/src/app/profile/profile.component.ts
+++ gitsearch/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { FormsModule } from '@angular/forms'
+import { Repos } from '../repos';
 
 
 @Component({
---
+++ gitsearch/src/app/repos.ts
@@ -0,0 +1,6 @@
+export class Repos {
+    constructor(
+        public name: string, 
+        public repo_url: string, 
+        public description: string) {}
+}
