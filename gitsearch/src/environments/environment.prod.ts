dc2aa24a226af499eb8fe918745f3d2a66e7c931
Christin-nandy <achiengchristine14@gmail.com>
2020-06-25 00:24:13 +0300

    update environment folder

--- gitsearch/src/environments/environment.prod.ts
+++ gitsearch/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '7854e8b34e8ab80d3b12ab8854d0011ad07f6ae8'
 };
--- gitsearch/src/environments/environment.ts
+++ gitsearch/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '7854e8b34e8ab80d3b12ab8854d0011ad07f6ae8'
 };
 
 /*
